9a333c88bc049c1f7a96f7ed92a1ea5630835fba
Heinrich Klopper <heinrich.klopper@tuks.co.za>
2026-09-07 10:39:02 +0200

    ToDoTask implemented partially: getIterator unimplemented

---
+++ src/cpp/ToDoTask.cpp
@@ -0,0 +1,94 @@
+#include "../headers/ToDoTask.h"
+#include <iostream>
+
+ToDoTask::ToDoTask(const string &name, 
+		const string &description, 
+		State* state,
+		int priority, 
+		const string &dueDate) : 
+			name(name), 
+			description(description), 
+			state(state),
+			priority(priority), 
+			dueDate(dueDate)
+{
+	if (!state)
+		throw "state cannot be null";
+}
+
+const string& ToDoTask::getName() const
+{
+	return name;
+}
+
+const string& ToDoTask::getDescription() const 
+{
+	return description;
+}
+
+const State* ToDoTask::getState() const
+{
+	return state;
+}
+
+int ToDoTask::getPriority() const 
+{
+	return priority;
+}
+
+const string& ToDoTask::getDueDate() const
+{
+	return dueDate;
+}
+
+void ToDoTask::execute() 
+{
+	cout << "Task " << name << " is due " << dueDate << endl;
+}
+
+const string& ToDoTask::getStatus() const 
+{
+	return state->getName();
+}
+
+void ToDoTask::start()
+{
+	state->start(this);
+}
+
+void ToDoTask::pause()
+{
+	state->pause(this);
+}
+
+void ToDoTask::complete()
+{
+	state->complete(this);
+}
+
+void ToDoTask::cancel()
+{
+	state->cancel(this);
+}
+
+void ToDoTask::setState(State* state)
+{
+	delete this->state;
+	if (!state)
+		throw "state cannot be null";
+	else
+		this->state = state;
+}
+
+ItemIterator* ToDoTask::createIterator(const string& type)
+{
+	return NULL; // implement this
+}
+
+ToDoTask::~ToDoTask()
+{
+	delete state;
+}
+
+
+
